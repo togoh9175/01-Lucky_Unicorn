0c4afdaccebc7d604114cc4ec13bb66a0e356310
togoh9175 <>
2021-04-09 00:46:33 +0000

    finished token, fixed looping

--- 05_token_generator_v4.py
+++ 05_token_generator_v4.py
@@ -8,21 +8,22 @@ balance = STARTING_BALANCE
 # Testing loop to generate 20 tokens
 for item in range(0,10):
   chosen_num = random.randint(1,100)
+  print(chosen_num)
 
-# Adjust balance
-if 1 <= chosen_num <= 5:
-  chosen = "unicorn"
-  balance += 4
+  # Adjust balance
+  if 1 <= chosen_num <= 5:
+    chosen = "unicorn"
+    balance += 4
 
-elif 6 <= chosen_num <= 36:
-  chosen = "donkey"
-  balance -= 1
-else:
-  if chosen_num % 2 == 0:
-    chosen = "horse"
+  elif 6 <= chosen_num <= 36:
+    chosen = "donkey"
+    balance -= 1
   else:
-    chosen = "zebra"
-  balance -= 0.5
+    if chosen_num % 2 == 0:
+      chosen = "horse"
+    else:
+      chosen = "zebra"
+    balance -= 0.5
 
-print ("you got a {}. Your balance is" "${:.2f}".format(chosen, balance))
-print()
+  print ("you got a {}. Your balance is" " ${:.2f}".format(chosen, balance))
+  print()
